9cae0810fb51d4fddf7a8daa412ef3e27e52ac49
Anton <anton.esev@mail.ru>
2023-06-17 15:48:08 +0300

    sem9

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задайте значения M и N. Напишите программу, которая
+ выведет все натуральные числа по убыванию в промежутке от M до N  */
+
+
+using System;
+using static System.Console;
+
+WriteLine("Введите натуральное число N:");
+int number = int.Parse(ReadLine());
+void PrintNumbers (int number)
+{
+    if (number < 0) Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Write("{0,4}", number);
+    PrintNumbers (number - 1);
+}
+
+PrintNumbers(number);
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N. 
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+using System;
+using static System.Console;
+
+WriteLine("Введите  число M:");
+int M = int.Parse(ReadLine());
+
+WriteLine("Введите число M:");
+int N = int.Parse(ReadLine());
+
+void GapNumberSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    GapNumberSum(M, N, sum);
+}
+
+GapNumberSum(M, N, 0);
